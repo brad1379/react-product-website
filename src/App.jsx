5905c5201c074b09dc9f099521b306edcdd8b312
Brad Arnold <brad1379@gmail.com>
2026-07-23 19:56:25 -0500

    reworked routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import About from "./pages/About"
+import NavBar from "./components/NavBar"
 import Home from "./pages/Home"
 import Admin from "./pages/Admin"
-import ProductContainer from "./pages/ProductContainer"
 import ProductList from "./pages/ProductList"
 import ProductCard from "./pages/ProductCard"
-import ProductForm from "./pages/ProductForm"
-
-
 import './App.css'
 
 function App() {
 
   return (
     <BrowserRouter>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/products" element={<ProductContainer/>}>
-          <Route path="new" element={<ProductForm/>}/>
-          <Route path="" element={<ProductList/>}/>
+        <Route path="products" element={<ProductList/>}>
           <Route path=":id" element={<ProductCard/>}/>
         </Route>
-        <Route path="/admin" element={<Admin />}>
-          <Route path="products" element={<ProductList/>}/>
-          <Route path="products/:id" element={<ProductCard/>}/>
-          <Route path="products/new" element={<ProductForm/>}/>
-        </Route>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/admin" element={<Admin />}/>
       </Routes>
     </BrowserRouter>
   )
